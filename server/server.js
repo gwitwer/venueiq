5b1a0e10e72582048670bf14f4638461795ee762
gwitwer <gdavid.witwer@gmail.com>
2018-06-13 09:14:44 -0400

    add some changes for aws. Improve signin layout

--- server/server.js
+++ server/server.js
@@ -116,7 +116,6 @@ const renderFullPage = (html, initialState) => {
 
         ${isProdMode ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
         <link rel="stylesheet" href="https://unpkg.com/react-select@1.2.1/dist/react-select.css">
         <style>
           body {
