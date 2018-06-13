42ce568d2c9fbc70ad0154dc0a0e4aff30a12c14
gwitwer <gdavid.witwer@gmail.com>
2018-06-13 09:52:36 -0400

    error handling

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,10 +11,14 @@ export function getUser(req, res) {
 }
 
 export function updateUser(req, res) {
-  User.findOneAndUpdate({ cuid: req.session.user.cuid }, req.body, (err, user) => {
-    if (err) {
-      console.log(err); // eslint-disable-line no-console
-    }
-    res.status(200).send({ success: !err, err, user });
-  });
+  if (req.session.user) {
+    User.findOneAndUpdate({ cuid: req.session.user.cuid }, req.body, (err, user) => {
+      if (err) {
+        console.log(err); // eslint-disable-line no-console
+      }
+      res.status(200).send({ success: !err, err, user });
+    });
+  } else {
+    res.status(200).send({ err: 'Authorization error. Please log in again to continue.' });
+  }
 }
